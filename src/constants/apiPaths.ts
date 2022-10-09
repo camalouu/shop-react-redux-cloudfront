9d31acabcf04a4cc2010549d4e8fe8309e979dcd
Zafar <camalouu@gmail.com>
2022-10-09 18:16:46 +0500

    import product csv file implemented

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://pwwgz6eu6e.execute-api.eu-north-1.amazonaws.com/dev",
+  bff: "https://m3a8z91udj.execute-api.eu-north-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
