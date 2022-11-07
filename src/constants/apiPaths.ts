228e84418b5ce0ddbad6958a4a0c9a472fa83560
Zafar <camalouu@gmail.com>
2022-11-07 18:03:13 +0500

    Cart api added

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://pwwgz6eu6e.execute-api.eu-north-1.amazonaws.com/dev",
   bff: "https://m3a8z91udj.execute-api.eu-north-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://camalouu-cart-api-dev.eu-north-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
